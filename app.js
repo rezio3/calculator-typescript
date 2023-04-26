ad95fac9001b2b24f5283762a7ea431259fcb429
Joszko <jakub.rezler96@gmail.com>
2023-04-26 16:50:39 +0200

    add sample code in app.ts and compile to app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var resultSpan = document.getElementById("result-span");
+var number1 = document.getElementById("number1");
+number1.addEventListener("click", function () {
+    console.log("1");
+});
---
+++ app.ts
@@ -0,0 +1,6 @@
+const resultSpan = document.getElementById("result-span");
+const number1 = document.getElementById("number1")! as HTMLButtonElement;
+
+number1.addEventListener("click", () => {
+	console.log("1");
+});
