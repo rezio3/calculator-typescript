4c1799391e6672ffbb95eeba9267fa50d45b3197
Joszko <jakub.rezler96@gmail.com>
2023-04-26 17:22:22 +0200

    get elements as an object in app.ts

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-var zero = document.getElementById("number0");
-var one = document.getElementById("number1");
-var two = document.getElementById("number2");
-var three = document.getElementById("number3");
-var four = document.getElementById("number4");
-var five = document.getElementById("number5");
-var six = document.getElementById("number6");
-var seven = document.getElementById("number7");
-var eight = document.getElementById("number8");
-var nine = document.getElementById("number9");
+var numbers = document.querySelectorAll(".number");
 var resultSpan = document.getElementById("result-span");
-one.addEventListener("click", function () {
-    if (resultSpan.innerHTML !== "0") {
-        resultSpan.innerHTML += "1";
-    }
-    else {
-        resultSpan.innerHTML = "1";
-    }
+numbers.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (resultSpan.textContent !== "0") {
+            resultSpan.innerHTML += button.innerHTML;
+        }
+        else {
+            resultSpan.innerHTML = button.innerHTML;
+        }
+    });
 });
--- app.ts
+++ app.ts
@@ -1,20 +1,17 @@
-const zero = document.getElementById("number0")! as HTMLButtonElement;
-const one = document.getElementById("number1")! as HTMLButtonElement;
-const two = document.getElementById("number2")! as HTMLButtonElement;
-const three = document.getElementById("number3")! as HTMLButtonElement;
-const four = document.getElementById("number4")! as HTMLButtonElement;
-const five = document.getElementById("number5")! as HTMLButtonElement;
-const six = document.getElementById("number6")! as HTMLButtonElement;
-const seven = document.getElementById("number7")! as HTMLButtonElement;
-const eight = document.getElementById("number8")! as HTMLButtonElement;
-const nine = document.getElementById("number9")! as HTMLButtonElement;
+const numbers = document.querySelectorAll(
+	".number"
+)! as NodeListOf<HTMLButtonElement>;
 
-const resultSpan = document.getElementById("result-span")! as HTMLSpanElement;
+const resultSpan: HTMLSpanElement = document.getElementById(
+	"result-span"
+)! as HTMLSpanElement;
 
-one.addEventListener("click", () => {
-	if (resultSpan.innerHTML !== "0") {
-		resultSpan.innerHTML += "1";
-	} else {
-		resultSpan.innerHTML = "1";
-	}
+numbers.forEach((button) => {
+	button.addEventListener("click", () => {
+		if (resultSpan.textContent !== "0") {
+			resultSpan.innerHTML += button.innerHTML;
+		} else {
+			resultSpan.innerHTML = button.innerHTML;
+		}
+	});
 });
