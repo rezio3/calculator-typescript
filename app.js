409353c297fc53b0298d2573e5ab2b2635343ec3
Joszko <jakub.rezler96@gmail.com>
2023-04-26 17:08:50 +0200

    get buttons elements in app.ts

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+var zero = document.getElementById("number0");
+var one = document.getElementById("number1");
+var two = document.getElementById("number2");
+var three = document.getElementById("number3");
+var four = document.getElementById("number4");
+var five = document.getElementById("number5");
+var six = document.getElementById("number6");
+var seven = document.getElementById("number7");
+var eight = document.getElementById("number8");
+var nine = document.getElementById("number9");
 var resultSpan = document.getElementById("result-span");
-var number1 = document.getElementById("number1");
-number1.addEventListener("click", function () {
-    console.log("1");
+one.addEventListener("click", function () {
+    if (resultSpan.innerHTML !== "0") {
+        resultSpan.innerHTML += "1";
+    }
+    else {
+        resultSpan.innerHTML = "1";
+    }
 });
--- app.ts
+++ app.ts
@@ -1,6 +1,20 @@
-const resultSpan = document.getElementById("result-span");
-const number1 = document.getElementById("number1")! as HTMLButtonElement;
+const zero = document.getElementById("number0")! as HTMLButtonElement;
+const one = document.getElementById("number1")! as HTMLButtonElement;
+const two = document.getElementById("number2")! as HTMLButtonElement;
+const three = document.getElementById("number3")! as HTMLButtonElement;
+const four = document.getElementById("number4")! as HTMLButtonElement;
+const five = document.getElementById("number5")! as HTMLButtonElement;
+const six = document.getElementById("number6")! as HTMLButtonElement;
+const seven = document.getElementById("number7")! as HTMLButtonElement;
+const eight = document.getElementById("number8")! as HTMLButtonElement;
+const nine = document.getElementById("number9")! as HTMLButtonElement;
 
-number1.addEventListener("click", () => {
-	console.log("1");
+const resultSpan = document.getElementById("result-span")! as HTMLSpanElement;
+
+one.addEventListener("click", () => {
+	if (resultSpan.innerHTML !== "0") {
+		resultSpan.innerHTML += "1";
+	} else {
+		resultSpan.innerHTML = "1";
+	}
 });
